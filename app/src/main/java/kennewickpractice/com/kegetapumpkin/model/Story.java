666804a54d5810387c11aba0edc1c2d0eceec41b
Ken Wagner <ken_xwagner@hotmail.com>
2015-11-12 23:23:04 -0800

    Ready to publish

--- app/src/main/java/kennewickpractice/com/kegetapumpkin/model/Story.java
+++ app/src/main/java/kennewickpractice/com/kegetapumpkin/model/Story.java
@@ -32,14 +32,14 @@ public class Story {
         mPages[3]=new Page(
                 R.mipmap.page3,
                 "You are in the corn maze, but it is much bigger than K and E thought it would be. You can go back or finish the Corn Maze. What should we do?",
-                new Choice("Go back to the pumpkin patch", 2),
+                new Choice("Go back to the Pumpkin Patch", 2),
                 new Choice("Finish the corn maze", 6));
 
         mPages[4]=new Page(
                 R.mipmap.page4,
-                "You are riding the tractor and it stops two times. Which one would you like to take?",
-                new Choice("Get off at the first stop", 5),
-                new Choice("Get off at the second stop", 7));
+                "You are riding the tractor and it stops two times, %1$s. Which one would you like to take?",
+                new Choice("Go to the first stop", 5),
+                new Choice("Go to the second stop", 7));
 
         mPages[5]=new Page(
                 R.mipmap.page5,
@@ -55,25 +55,25 @@ public class Story {
 
         mPages[7]=new Page(
                 R.mipmap.page7,
-                "K and E are happy! You found the right pumpkin",
+                "You helped K and E find the right pumpkin, %1$s. K and E are happy with this one!",
                 new Choice("Buy it", 8),
                 new Choice("Go back to the tractor ride", 4));
 
         mPages[8]=new Page(
                 R.mipmap.page8,
-                "K and E hand over the pumpkin so that their parents can buy it. They are happy you came along. ",
+                "K and E hand over the pumpkin so that their parents can buy it. They are glad you came along. ",
                 new Choice("Continue", 9),
                 new Choice("Back to the giant pumpkin", 5));
 
         mPages[9]=new Page(
                 R.mipmap.page9,
-                "Thanks your you help! K and E would like to thank you.",
+                "Thanks your you help, %1$s! K and E would like to thank you.",
                 new Choice("The last page", 10),
                 new Choice("Coffee time", 1));
 
         mPages[10]=new Page(
                 R.mipmap.page10,
-                "Happy Halloween");
+                "Happy Halloween, %1$s!");
     }
 
     public Page getPage(int pageNumber) {
